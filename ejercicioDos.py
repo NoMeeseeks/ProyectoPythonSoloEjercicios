b3f39c36664da21f6ec811030fa49d6a2de08ebe
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-01-24 08:43:18 -0500

    Primeros ejercicios para el repositorio

---
+++ ejercicioDos.py
@@ -0,0 +1,2 @@
+print("Ejercicio numero 2")
+print("Leer la siguiente lista que contiene numero y palabras, cuando encuentre un numero sumara el valor del numero y cuando sea una palabra sumara 1")
---
+++ ejercicioUno.py
@@ -0,0 +1,9 @@
+print("Ejercicio numero 1")
+print("Imprimir solo lo numeros pares hasta el numero 100")
+
+#numero que se va ingresar
+n=100
+
+for i in range (1,100):
+    if i %2 == 0:
+        print("Este numero es par: ", i)
