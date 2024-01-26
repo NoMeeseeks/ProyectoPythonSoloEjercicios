964d4d10abdbec89275ec52218792c0465ec2944
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-01-26 07:38:23 -0500

    Resolucion del segundo ejercicio

--- ejercicioDos.py
+++ ejercicioDos.py
@@ -1,2 +1,12 @@
 print("Ejercicio numero 2")
-print("Leer la siguiente lista que contiene numero y palabras, cuando encuentre un numero sumara el valor del numero y cuando sea una palabra sumara 1")
+print("Leer la siguiente lista que contiene numero y palabras, cuando encuentre un numero sumar el valor del numero y cuando sea una palabra sumara 1")
+cosasGuardadas = ["Pelota de futbol",10,"Cargador de telefono",2000,"Avion de juguete",45968,56,True]
+contadorPalabras=0
+contadorNumeros=0
+for i in cosasGuardadas:
+    if type(i) == str:
+        contadorPalabras += 1
+    elif type(i) == int:
+        contadorNumeros += i
+print("Las palabras en la lista son: ", contadorPalabras)
+print("Los numeros en la lista son: ", contadorNumeros)
