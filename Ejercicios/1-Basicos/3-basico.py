3034ace746992de6eccb90f938c9c498d6cf3c94
Xavier <Xavier.e.santamaria.ramirez@gmail.com>
2024-06-30 19:21:57 -0500

    Un Ejercicio por dia Dia: 1

---
+++ Ejercicios/1-Basicos/3-basico.py
@@ -0,0 +1,5 @@
+#Ingrese por pantalla su nombre y edad para imprimirlo con un mensaje de este sera el mejor dia de tu vida
+
+nombre = input("Ingrese su nombre: ")
+edad = input("Ingrese su edad: ")
+print( nombre.upper(),"tiene", edad, "años")
